8152b5f2538aaf2f2fdc7c6a4f21e013f59ac1f7
Mateen Mushrif <mateenmushrif04@gmail.com>
2026-06-23 11:59:29 +0530

    Allow Vercel frontend origin

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://hrdashobodo.vercel.app"
+        "https://hrdashobodo-l2lfsaqay-mateens-projects-f987f120.vercel.app"
     ], // your Next.js port
     credentials: true,
 }));
